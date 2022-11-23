19b3400020b79f3f953220709d6324b08952c828
PavelSurint <pavelsurint@gmail.com>
2022-11-23 19:37:25 +0300

    Добавили дерикторию idea в игнор

---
+++ conftest.py
@@ -0,0 +1,14 @@
+import pytest
+from selene.support.shared import browser
+
+
+@pytest.fixture(scope="class")
+def open_browser():
+    browser.config.window_height = 950
+    browser.config._window_width = 1600
+    browser.open('https://google.com/ncr')
+    browser.element("[id='L2AGLb']").click()  # подтвердил использование куки
+
+    yield
+
+    browser.quit()
---
+++ google_search_test.py
@@ -0,0 +1,15 @@
+from selene.support.shared import browser
+from selene import be, have
+import pytest
+
+
+class TestGoogleSearch():
+    def test_selene_search(self, open_browser):
+        browser.element('[name="q"]').should(be.blank).type(
+            'selene python').press_enter()  # уточнил поиск, а то не выдавал из-за локации
+        browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
+
+    def test_negative_search(self, open_browser):
+        browser.element('[name="q"]').set('6576575675675675675675gfhfgh').press_enter()
+        browser.element('[id="center_col"]').should(
+            have.text('Your search - 6576575675675675675675gfhfgh - did not match any documents.'))
